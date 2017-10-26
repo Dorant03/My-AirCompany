3cb86065d69772e04ec1ad2ec696ff3ee9639d31
OlenaLNU1999 <31958821+OlenaLNU1999@users.noreply.github.com>
2017-10-26 19:50:27 +0300

    Create Travel.h

---
+++ Travel.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include "Airport.h"
+#include "Aircraft.h"
+#include "Passenger.h"
+
+using namespace std;
+
+class Travel
+{
+protected:
+	Airport port_start;
+	Airport port_end;
+	string time_start;
+	string time_end;
+	Aircraft craft_name;
+	Passenger *array_passenger;
+	int number_passenger;
+public:
+	Travel();
+	Travel(int);
+	Travel(ifstream &fin);
+	Travel(const Travel &);
+	Travel & operator=(const Travel &ob);
+	~Travel();
+};
