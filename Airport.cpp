1f7d8ee3290e3ad8ce2e1781ffb54cebae0fc4a4
Dinara227 <32163613+Dinara227@users.noreply.github.com>
2017-10-26 22:02:04 +0300

    Airport.cpp

---
+++ Airport.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "Airport.h"
+
+using namespace std;
+
+Airport::Airport()
+{
+	name="\0";
+	city="\0";
+	country="\0";
+}
+
+Airport::~Airport()
+{ }
