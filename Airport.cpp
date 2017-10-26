c6a71b7208c56f0841a7f9d012b2d05c0428659d
Dinara227 <32163613+Dinara227@users.noreply.github.com>
2017-10-26 23:17:42 +0300

    Update Airport.cpp,new methods

--- Airport.cpp
+++ Airport.cpp
@@ -11,6 +11,30 @@ Airport::Airport()
 	city="\0";
 	country="\0";
 }
-
+Airport::Airport(int k)
+{
+	cout<<"Name airport: "; getline(cin, name);
+	cout<<"Name city: "; getline(cin, city);
+	cout<<"Name country: "; getline(cin, country);
+}
+Airport::Airport(ifstream &fin)
+{
+	getline(fin, name);
+	getline(fin, city);
+	getline(fin, country);
+}
+Airport::Airport(const Airport &ob)
+{
+	name=ob.name;
+	city=ob.city;
+	country=ob.country;
+}
+Airport& Airport::operator=(const Airport &ob)
+{
+	name=ob.name;
+	city=ob.city;
+	country=ob.country;
+	return *this;
+}
 Airport::~Airport()
 { }
