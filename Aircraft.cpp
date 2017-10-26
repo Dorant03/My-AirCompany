cce92018496eb183395e9dc1179a87c07ededc80
Mirroslava <31761632+Mirroslava@users.noreply.github.com>
2017-10-26 22:04:56 +0300

    Add files via upload

---
+++ Aircraft.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "Aircraft.h"
+
+using namespace std;
+
+Aircraft::Aircraft()
+{
+}
+Aircraft::Aircraft(int k)
+{
+}
+Aircraft::Aircraft(ifstream &fin)
+{
+}
+Aircraft::Aircraft(const Aircraft &ob)
+{
+}
+Aircraft& Aircraft::operator=(const Aircraft &ob)
+{
+}
+string Aircraft::getName()
+{
+}
+int Aircraft::getN()
+{
+}
+string * Aircraft::getPlace()
+{
+}
+void Aircraft::info()
+{
+}
+Aircraft::~Aircraft()
+{
+}
